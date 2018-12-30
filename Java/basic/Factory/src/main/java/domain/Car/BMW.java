445122983b5e1343a122bbfe84ee25765fe28a57
kmluns <jegristan@gmail.com>
2018-12-30 14:09:03 +0300

    created Factory basic pattern

---
+++ Java/basic/Factory/src/main/java/domain/Car/BMW.java
@@ -0,0 +1,15 @@
+package domain.Car;
+
+/**
+ * Created by kmluns on 30.12.2018
+ */
+public class BMW  extends  Car{
+
+    public BMW(){
+        this.model = "BMW 330i xDrive";
+        this.door = 5;
+        this.engineHp = 251;
+        this.producedYear = 2019;
+    }
+
+}
